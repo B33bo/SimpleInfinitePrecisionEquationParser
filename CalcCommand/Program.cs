d6deb08e8fbb5bc7516d8253d1b339ec93571a2b
Brandon Piper <brandonpiper1@gmail.com>
2023-05-27 17:12:33 +0100

    Changes to the command

--- CalcCommand/Program.cs
+++ CalcCommand/Program.cs
@@ -1,40 +1,31 @@
 ﻿using SIPEP;
+using System.Numerics;
+using System.Text;
 
 namespace CalcCommand;
 
 public static class Program
 {
-    public static bool KeepOpen = true;
+    private static bool KeepOpen = true;
     public static void Main(string[] args)
     {
         FunctionLoader.ReloadFunctions();
-#nullable disable
+
         FunctionLoader.loadedFunctions?.Add((new FunctionAttribute("Exit"), typeof(Program).GetMethod("Exit")));
         FunctionLoader.loadedFunctions?.Add((new FunctionAttribute("TextColor"), typeof(Program).GetMethod("TextColor")));
         FunctionLoader.loadedFunctions?.Add((new FunctionAttribute("BGColor"), typeof(Program).GetMethod("BGColor")));
-#nullable enable
 
         if (args.Length == 0)
         {
-            while (KeepOpen)
-            {
-                string? input = Console.ReadLine();
-
-                if (input is null)
-                    continue;
-
-                try
-                {
-                    Console.WriteLine(new Equation(input).Solve());
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Error");
-                }
-            }
+            ForeverMode();
             return;
         }
 
+        CommandMode(args);
+    }
+
+    private static void CommandMode(string[] args)
+    {
         Equation e = new("");
         string[] equations = args[0].Split(';');
 
@@ -42,15 +33,79 @@ public static class Program
         {
             try
             {
+                if (equations[i].StartsWith("solve"))
+                {
+                    Console.WriteLine(GetSolutionString(equations[i], e));
+                    continue;
+                }
+
                 e.LoadString(equations[i]);
-                if (i == equations.Length - 1)
-                    Console.WriteLine(e.Solve());
+                Console.WriteLine(e.Solve());
             }
             catch (Exception)
             {
                 Console.WriteLine("Error");
             }
         }
+    }
+
+    private static void ForeverMode()
+    {
+        while (KeepOpen)
+        {
+            string? input = Console.ReadLine();
+
+            if (input is null)
+                continue;
+
+            try
+            {
+                if (input.StartsWith("solve"))
+                {
+                    Console.WriteLine(GetSolutionString(input, new Equation("")));
+                    continue;
+                }
+
+                Console.WriteLine(new Equation(input).Solve());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+
+    private static string GetSolutionString(string s, Equation e)
+    {
+        // format: solve,varName,max,depth:
+        var argsString = s.Split(':')[0];
+        var args = argsString.Split(',');
+
+        e.LoadString(s[(argsString.Length + 1)..]);
+
+        double max = 100;
+        int depth = 20;
+        BigRational cutoff = BigRational.Parse("0.1");
+        bool realOnly = false;
+
+        if (args.Length >= 3)
+            max = double.Parse(args[2]);
+        if (args.Length >= 4)
+            depth = int.Parse(args[3]);
+        if (args.Length >= 5)
+            cutoff = BigRational.Parse(args[4]);
+        if (args.Length >= 6)
+            realOnly = bool.Parse(args[5]);
+
+        var solutions = e.FindSolutions(args[1], max, depth, cutoff, realOnly);
+
+        if (solutions.Length == 0)
+            return "No Solutions";
+
+        StringBuilder finalStr = new();
+        for (int i = 0; i < solutions.Length; i++)
+            finalStr.Append(solutions[i].ToString() + ", ");
+        return finalStr.Remove(finalStr.Length - 2, 2).ToString();
     }
 
     public static BigComplex Exit(params BigComplex[] args)
